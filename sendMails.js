8f807561391b29710f12d12dea1fb269ba7db2fd
EstelleR <roges.estelle@gmail.com>
2022-08-06 17:18:22 +0200

    mail sending enabled

---
+++ sendMails.js
@@ -0,0 +1,25 @@
+const sendMail = () => {
+  var personName = document.getElementById("personName").value;
+  var personMail = document.getElementById("personMail").value;
+  var mailTopic = document.getElementById("topic").value;
+  var personMessage = document.getElementById("personMessage").value;
+  emailjs
+    .send("gmailService_teru", "teru_basic_template", {
+      from_name: personName,
+      to_name: "Teru",
+      subject: mailTopic,
+      message: personMessage,
+      reply_to: personMail,
+    })
+    .then(
+      function (response) {
+        console.log("SUCCESS!", response.status, response.text);
+           alert("Votre message a bien été envoyé!");
+      },
+      function (error) {
+        console.log("FAILED...", error);
+           alert("Désolée, il y a eu un souci au niveau de l'envoi...")
+      }
+    );
+ 
+};
